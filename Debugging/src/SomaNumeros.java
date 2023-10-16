09274e589e6093ea65c02aa67edf1d2bde9e4598
brunotomaz-dev <tomaz.bruno@gmail.com>
2023-10-16 16:58:20 -0300

    :tada: Uso do debugger
    Exercício concluido om sucesso

---
+++ Debugging/src/SomaNumeros.java
@@ -0,0 +1,13 @@
+public class SomaNumeros {
+  /*
+  * Retorna a soma de dois números
+  * @param numA um número inteiro
+  * @param numB outro numero inteiro
+  * @return a soma entre dois números
+  */
+
+  public static int retornaSomaEntreDoisNumeros(int numA, int numB) {
+    int soma = numA + numB;
+    return soma;
+  }
+}
